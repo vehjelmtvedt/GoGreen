369169e14532205c1ed63bec74a7c386bb610e10
alemoraru <A.Moraru@student.tudelft.nl>
2019-02-19 23:04:29 +0100

    update gui - sample login/signup forms with close window handling

--- src/GUI/src/sample/Main.java
+++ src/GUI/src/sample/Main.java
@@ -9,17 +9,15 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
-public class Main extends Application{
+public class Main extends Application {
+
+    Stage window;
 
     private GridPane createRegistrationForm() {
         GridPane gridPane = new GridPane();
@@ -41,81 +39,141 @@ public class Main extends Application{
         return gridPane;
     }
 
-    private void addUIControls(GridPane gridPane) {
-
-        //add the header of the form
-        Label headerLabel = new Label("Registration Form");
+    private void addUIControls(GridPane gridPane, int type) {
+        Label headerLabel = new Label("Sign-up Form");
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
-        gridPane.add(headerLabel, 0,0,2,1);
+
+        gridPane.add(headerLabel, 0, 0, 2, 1);
         GridPane.setHalignment(headerLabel, HPos.CENTER);
-        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+        GridPane.setMargin(headerLabel, new Insets(20, 0, 20, 0));
 
-        // Add Email Label
-        Label emailLabel = new Label("Email ID : ");
-        gridPane.add(emailLabel, 0, 1);
+        if (type == 1) {
+            // Add Email Label
+            Label emailLabel = new Label("*Email ID : ");
+            gridPane.add(emailLabel, 0, 1);
 
-        // Add Email Text Field
-        TextField emailField = new TextField();
-        emailField.setPrefHeight(40);
-        gridPane.add(emailField, 1, 1);
+            // Add Email Text Field
+            TextField emailField = new TextField();
+            emailField.setPrefHeight(40);
+            gridPane.add(emailField, 1, 1);
 
-        // Add Password Label
-        Label passwordLabel = new Label("Password : ");
-        gridPane.add(passwordLabel, 0, 2);
+            // Add Password Label
+            Label passwordLabel = new Label("*Password : ");
+            gridPane.add(passwordLabel, 0, 2);
 
-        // Add Password Field
-        PasswordField passwordField = new PasswordField();
-        passwordField.setPrefHeight(40);
-        gridPane.add(passwordField, 1, 2);
+            // Add Password Field
+            PasswordField passwordField = new PasswordField();
+            passwordField.setPrefHeight(40);
+            gridPane.add(passwordField, 1, 2);
 
-        // Add First Name Label
-        Label firstNameLabel = new Label("First Name : ");
-        gridPane.add(firstNameLabel, 0,3);
+            // Add First Name Label
+            Label firstNameLabel = new Label("*First Name : ");
+            gridPane.add(firstNameLabel, 0, 3);
 
-        // Add First Name Text Field
-        TextField firstNameField = new TextField();
-        firstNameField.setPrefHeight(40);
-        gridPane.add(firstNameField, 1,3);
+            // Add First Name Text Field
+            TextField firstNameField = new TextField();
+            firstNameField.setPrefHeight(40);
+            gridPane.add(firstNameField, 1, 3);
 
-        // Add Last Name Label
-        Label lastNameLabel = new Label("Last Name : ");
-        gridPane.add(lastNameLabel, 0,4);
+            // Add Last Name Label
+            Label lastNameLabel = new Label("*Last Name : ");
+            gridPane.add(lastNameLabel, 0, 4);
 
-        // Add Last Name Text Field
-        TextField lastNameField = new TextField();
-        lastNameField.setPrefHeight(40);
-        gridPane.add(lastNameField, 1,4);
+            // Add Last Name Text Field
+            TextField lastNameField = new TextField();
+            lastNameField.setPrefHeight(40);
+            gridPane.add(lastNameField, 1, 4);
 
-        // Add Age Label
-        Label ageLabel = new Label("Age : ");
-        gridPane.add(ageLabel, 0,5);
+            // Add Age Label
+            Label ageLabel = new Label("Age : ");
+            gridPane.add(ageLabel, 0, 5);
 
-        // Add Age Text Field
-        TextField ageField = new TextField();
-        ageField.setPrefHeight(40);
-        gridPane.add(ageField, 1,5);
+            // Add Age Text Field
+            TextField ageField = new TextField();
+            ageField.setPrefHeight(40);
+            gridPane.add(ageField, 1, 5);
 
-        // Add Submit Button
-        Button submitButton = new Button("Submit");
-        submitButton.setPrefHeight(40);
-        submitButton.setDefaultButton(true);
-        submitButton.setPrefWidth(100);
-        gridPane.add(submitButton, 0, 6, 2, 1);
-        GridPane.setHalignment(submitButton, HPos.CENTER);
-        GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
+            // Add Submit Button
+            Button submitButton = new Button("Submit");
+            submitButton.setPrefHeight(40);
+            submitButton.setDefaultButton(true);
+            submitButton.setPrefWidth(100);
+            gridPane.add(submitButton, 0, 6, 2, 1);
+            GridPane.setHalignment(submitButton, HPos.CENTER);
+            GridPane.setMargin(submitButton, new Insets(20, 0, 20, 0));
+        }
+        if (type == 2) {
+            headerLabel.setText("Login form");
+            // Add Email Label
+            Label emailLabel = new Label("Email : ");
+            gridPane.add(emailLabel, 0, 1);
+
+            // Add Email Text Field
+            TextField emailField = new TextField();
+            emailField.setPrefHeight(40);
+            gridPane.add(emailField, 1, 1);
+
+            // Add Password Label
+            Label passwordLabel = new Label("Password : ");
+            gridPane.add(passwordLabel, 0, 2);
+
+            // Add Password Field
+            PasswordField passwordField = new PasswordField();
+            passwordField.setPrefHeight(40);
+            gridPane.add(passwordField, 1, 2);
+
+            // Add Submit Button
+            Button loginButton = new Button("Login");
+            loginButton.setPrefHeight(40);
+            loginButton.setDefaultButton(true);
+            loginButton.setPrefWidth(100);
+            gridPane.add(loginButton, 0, 3, 2, 1);
+            GridPane.setHalignment(loginButton, HPos.CENTER);
+            GridPane.setMargin(loginButton, new Insets(20, 0, 20, 0));
+        }
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        primaryStage.setTitle("Login/Sign-up Page");
+        //setup main window
+        window = primaryStage;
+        window.setTitle("Login/Sign-up");
 
-        GridPane gridPane = createRegistrationForm();
-        addUIControls(gridPane);
-        Scene scene = new Scene(gridPane, 800, 500);
+        //close button handling
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
+        //create borderPane for quick form setup
+        BorderPane mainLayout = new BorderPane();
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //initialise forms
+        GridPane signupForm = createRegistrationForm();
+        GridPane loginForm = createRegistrationForm();
+
+        //add the ui controls to the form
+        addUIControls(signupForm, 1);
+        addUIControls(loginForm, 2);
+
+        //set top/right/bottom/center/left for main borderPane
+        mainLayout.setRight(signupForm);
+        mainLayout.setLeft(loginForm);
+
+        //set final primary stage aka window
+        Scene scene = new Scene(mainLayout, 300, 300);
+        window.setScene(scene);
+        window.setMaximized(true);
+        window.show();
+    }
+
+    private void closeProgram(){
+        boolean answer = ConfirmBox.display("Close request safety", "Are you sure you want to close this application?");
+        if(answer){
+            System.out.println("Contents are saved!(not true)");
+            window.close();
+        }
     }
     public static void main(String[] args) {
         launch(args);
