c5bd80613ebdfacd34b0a655829d5aef6c92cab2
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-02-20 16:41:02 +0100

    DB testing setup & dependency for testing

---
+++ src/test/java/Backend/data/DBServiceTest.java
@@ -0,0 +1,19 @@
+package Backend.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureDataMongo
+public class DBServiceTest {
+    @Autowired
+    DBService dbService;
+}
