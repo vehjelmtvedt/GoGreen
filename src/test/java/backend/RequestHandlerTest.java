350815c4d9f6a9485c2f1d9863c5982afcff5bc1
Shashank Anand <s.anand-1@student.tudelft.nl>
2019-02-26 21:20:46 +0100

    fixed tests

--- src/test/java/backend/RequestHandlerTest.java
+++ src/test/java/backend/RequestHandlerTest.java
@@ -35,7 +35,7 @@ public class RequestHandlerTest
     public void testSignupExists()
     {
         Mockito.when(dbService.getUser(testUser.getEmail())).thenReturn(testUser);
-        assertEquals("user exists already", requestHandler.signupController(testUser));
+        assertEquals("email exists", requestHandler.signupController(testUser));
     }
 
     @Test
@@ -48,13 +48,14 @@ public class RequestHandlerTest
     @Test
     public void testLoginSuccess() {
         Mockito.when(dbService.grantAccess(testUser.getEmail(), testUser.getPassword())).thenReturn(true);
-        assertEquals("success", requestHandler.loginController(new LoginDetails(testUser.getEmail(), testUser.getPassword())));
+        Mockito.when(dbService.getUser(testUser.getEmail())).thenReturn(testUser);
+        assertEquals(testUser, requestHandler.loginController(new LoginDetails(testUser.getEmail(), testUser.getPassword())));
     }
 
     @Test
     public void testLoginFail() {
         Mockito.when(dbService.grantAccess(testUser.getEmail(), testUser.getPassword())).thenReturn(false);
-        assertEquals("failure", requestHandler.loginController(new LoginDetails(testUser.getEmail(), testUser.getPassword())));
+        assertEquals(null, requestHandler.loginController(new LoginDetails(testUser.getEmail(), testUser.getPassword())));
     }
 
 }
