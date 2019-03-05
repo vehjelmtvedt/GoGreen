573b9e42bb2d5f7a475861ff73aaacf29fbcda47
Vetle Hjelmtvedt <V.G.Hjelmtvedt@student.tudelft.nl>
2019-03-05 20:15:27 +0100

    no

--- src/main/java/frontend/Requests.java
+++ src/main/java/frontend/Requests.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 public class Requests {
 
     /**
