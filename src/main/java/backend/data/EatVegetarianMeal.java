9c7d34580e167a25d43ec36792a4f76938fa1d96
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-03-08 14:01:03 +0100

    created EatVegetarianMeal class

--- src/main/java/backend/data/EatVegetarianMeal.java
+++ src/main/java/backend/data/EatVegetarianMeal.java
@@ -1,4 +1,43 @@
 package backend.data;
 
-public class EatVegetarianMeal {
+import tools.CarbonCalculator;
+
+public class EatVegetarianMeal extends Activity {
+
+    public int dailylMeatAndDairyConsumption(User user) {
+        String meatAndDairyConsumption = user.getMeatAndDairyConsumption();
+        int dailylMeatAndDairyConsumption;
+        switch (meatAndDairyConsumption) {
+            case "above average":
+                dailylMeatAndDairyConsumption =
+                        CarbonCalculator.meatAndDairyConsumptionEmissions(
+                                CarbonCalculator.MeatAndDairyConsumption.ABOVE_AVERAGE) / 365;
+                break;
+            case "average" :
+                dailylMeatAndDairyConsumption =
+                        CarbonCalculator.meatAndDairyConsumptionEmissions(
+                                CarbonCalculator.MeatAndDairyConsumption.AVERAGE) / 365;
+                break;
+            case "below average" :
+                dailylMeatAndDairyConsumption =
+                        CarbonCalculator.meatAndDairyConsumptionEmissions(
+                                CarbonCalculator.MeatAndDairyConsumption.BELOW_AVERAGE) / 365;
+                break;
+            case "lacto-vegetarian" :
+                dailylMeatAndDairyConsumption =
+                        CarbonCalculator.meatAndDairyConsumptionEmissions(
+                                CarbonCalculator.MeatAndDairyConsumption.LACTO_VEGETARIAN) / 365;
+                break;
+            case "vegan" :
+                dailylMeatAndDairyConsumption =
+                        CarbonCalculator.meatAndDairyConsumptionEmissions(
+                                CarbonCalculator.MeatAndDairyConsumption.VEGAN) / 365;
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+        return dailylMeatAndDairyConsumption;
+    }
+
+
 }
