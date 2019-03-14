fc914a3651b5d2eb458ee0c1a1d89004f1522a2b
Vetle Hjelmtvedt <V.G.Hjelmtvedt@student.tudelft.nl>
2019-03-14 22:30:10 +0100

    Link navbar buttons to pages

--- src/main/java/frontend/controllers/NavPanelController.java
+++ src/main/java/frontend/controllers/NavPanelController.java
@@ -3,7 +3,11 @@ package frontend.controllers;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import frontend.Main;
+import frontend.StageSwitcher;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -13,9 +17,25 @@ import java.io.IOException;
 
 public class NavPanelController {
 
+    @FXML
+    private Button myProfile;
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button activity;
+
+    @FXML
+    private Button friends;
+
+
 
     public void initialize() {
-
+        //TODO: Add scene change to profile page
+        home.setOnAction(e -> StageSwitcher.sceneSwitch(Main.getPrimaryStage(), Main.getHomepage()));
+        activity.setOnAction(e -> StageSwitcher.sceneSwitch(Main.getPrimaryStage(), Main.getActivities()));
+        friends.setOnAction(e -> StageSwitcher.sceneSwitch(Main.getPrimaryStage(), Main.getFriendsPage()));
     }
 
 
