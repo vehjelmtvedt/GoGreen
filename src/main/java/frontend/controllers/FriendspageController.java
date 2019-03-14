b823755fbaf2fc8f819aa5fdbfe54f16f0613375
Vetle Hjelmtvedt <V.G.Hjelmtvedt@student.tudelft.nl>
2019-03-14 21:25:16 +0100

    Complete nav panel

--- src/main/java/frontend/controllers/FriendspageController.java
+++ src/main/java/frontend/controllers/FriendspageController.java
@@ -3,6 +3,7 @@ package frontend.controllers;
 import backend.data.User;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -37,13 +38,37 @@ public class FriendspageController {
     private JFXDrawer drawer;
 
     public void initialize() throws IOException {
+        initNavBar();
         getFriends.setOnAction(e -> fillFriendsPane());
-        //Load side nav panel
+    }
+
+    public void fillFriendsPane() {
+        VBox root = new VBox();
+
+        for (String username : user.getFriends()) {
+            Pane friendPane = new Pane();
+            friendPane.setMinHeight(50);
+            friendPane.prefWidthProperty().bind(root.widthProperty());
+            friendPane.setBackground(new Background(new BackgroundFill(Color.web("#4245f4"), CornerRadii.EMPTY, Insets.EMPTY)));
+            Label friendLabel = new Label(username);
+            friendPane.getChildren().add(friendLabel);
+            root.getChildren().add(friendPane);
+        }
+        scrollPaneFriends.setContent(root);
+        scrollPaneFriends.setPannable(true);
+    }
+
+
+
+    public static void setUser(User passedUser) {
+        user = passedUser;
+    }
+
+    public void initNavBar() throws IOException {
         VBox box = FXMLLoader.load(getClass().getResource("/frontend/fxmlPages/navigationpane.fxml"));
+
+        drawer.setVisible(false);
         drawer.setSidePane(box);
-
-
-
 
 
         //Handle nav panel
@@ -55,40 +80,20 @@ public class FriendspageController {
             //drawer.setVisible(true);
             if (drawer.isOpened()) {
                 drawer.close();
+                drawer.setVisible(false);
             } else {
                 drawer.open();
                 drawer.setVisible(true);
             }
         });
 
-    }
-
-    public void fillFriendsPane() {
-//        VBox root = new VBox();
-//
-//        for (String username : user.getFriends()) {
-//            Pane friendPane = new Pane();
-//            friendPane.setMinHeight(50);
-//            friendPane.prefWidthProperty().bind(root.widthProperty());
-//            friendPane.setBackground(new Background(new BackgroundFill(Color.web("#4245f4"), CornerRadii.EMPTY, Insets.EMPTY)));
-//            Label friendLabel = new Label(username);
-//            friendPane.getChildren().add(friendLabel);
-//            root.getChildren().add(friendPane);
-//        }
-//        scrollPaneFriends.setContent(root);
-//        scrollPaneFriends.setPannable(true);
-        if (drawer.isOpened()) {
-            drawer.close();
-        } else {
-            drawer.open();
-            drawer.setVisible(true);
-        }
-    }
-
-
-
-    public static void setUser(User passedUser) {
-        user = passedUser;
+        main.addEventHandler(MouseEvent.MOUSE_PRESSED, ev -> {
+            if (drawer.isOpened()) {
+                drawer.close();
+                burgerTask1.play();
+                drawer.setVisible(false);
+            }
+        });
     }
 
 
---
+++ src/main/java/frontend/controllers/NavPanelController.java
@@ -0,0 +1,61 @@
+package frontend.controllers;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+
+public class NavPanelController {
+
+    @FXML
+    public Button closeButtonNav;
+
+    private static JFXDrawer drawer;
+
+    private static JFXHamburger menu;
+
+
+    public void initialize() {
+
+    }
+
+
+    public void setup(JFXDrawer drawer, JFXHamburger menu) throws IOException {
+        VBox box = FXMLLoader.load(NavPanelController.class.getResource("/frontend/fxmlPages/navigationpane.fxml"));
+
+        drawer.setVisible(false);
+        drawer.setSidePane(box);
+
+        //Handle nav panel
+        HamburgerBasicCloseTransition burgerTask1 = new HamburgerBasicCloseTransition(menu);
+        burgerTask1.setRate(-1);
+        menu.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+            burgerTask1.setRate(burgerTask1.getRate() * -1);
+            burgerTask1.play();
+            //drawer.setVisible(true);
+            if (drawer.isOpened()) {
+                drawer.close();
+                drawer.setVisible(false);
+            } else {
+                drawer.setVisible(true);
+                drawer.open();
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+}
