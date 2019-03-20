855726bb2bbf0aa32cf90d840a07a2164b657747
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-20 22:48:33 +0100

    Add DateUtils tests

--- src/test/java/tools/DateUtilsTest.java
+++ src/test/java/tools/DateUtilsTest.java
@@ -1,7 +1,46 @@
 package tools;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class DateUtilsTest {
+    @Test
+    public void testDateUtilsInstance() {
+        Assert.assertNotNull(DateUtils.getInstance());
+    }
 
+    @Test
+    public void testDateToday() {
+        long today = Calendar.getInstance().getTime().getTime();
+        long today2 = DateUtils.getInstance().dateToday().getTime();
+
+        // Allow minor difference of 100ms
+        Assert.assertEquals(today, today2, 100);
+    }
+
+    @Test
+    public void testDateBefore() {
+        // Current date
+        Date today = DateUtils.getInstance().dateToday();
+
+        // Set calendar 1 week back from today
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(today);
+        calendar.add(Calendar.DATE, -7);
+
+        // set to midnight
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        Date result = DateUtils.getInstance().getDateBefore(today, DateUnit.WEEK);
+
+        Assert.assertEquals(calendar.getTime(), result);
+    }
 }
