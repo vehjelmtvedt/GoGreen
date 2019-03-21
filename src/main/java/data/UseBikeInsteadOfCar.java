2cd865dbfa09d47c4db7f3bebf96abe07d778c66
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-03-21 18:27:56 +0100

    Rename class TakeBikeInsteadOfCar to UseBikeInsteadOfCar

---
+++ src/main/java/data/UseBikeInsteadOfCar.java
@@ -0,0 +1,15 @@
+package data;
+
+/**
+ *
+ * @author Kostas Lyrakis
+ */
+public class UseBikeInsteadOfCar extends Activity{
+
+
+    @Override
+    public double calculateCarbonSaved(User user) {
+        // TODO
+        return 0;
+    }
+}
