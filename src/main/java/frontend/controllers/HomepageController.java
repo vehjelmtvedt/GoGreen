d10f1d4923d840aeaafdc931438bc6b1d98eb2fc
A.Moraru@student.tudelft.nl <A.Moraru@student.tudelft.nl>
2019-03-22 23:09:47 +0100

    added template piecharts on the homepage

--- src/main/java/frontend/controllers/HomepageController.java
+++ src/main/java/frontend/controllers/HomepageController.java
@@ -3,19 +3,23 @@ package frontend.controllers;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 
+import data.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Side;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import tools.ActivityQueries;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomepageController implements Initializable {
+    private static User loggedUser;
+
     @FXML
     private JFXHamburger menu;
     @FXML
@@ -25,13 +29,24 @@ public class HomepageController implements Initializable {
     @FXML
     private AnchorPane headerPane;
     @FXML
-    private PieChart chart1;
+    private PieChart chartCategory;
+    @FXML
+    private PieChart chartFood;
+    @FXML
+    private PieChart chartTransportation;
+    @FXML
+    private PieChart chartHousehold;
+    @FXML
+    private Label lblWelcome;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        chart1.setData(fillPieChart());
-        chart1.setLegendSide(Side.LEFT);
+        lblWelcome.setText("Welcome, " + loggedUser.getFirstName() + " "
+                + loggedUser.getLastName() + "! Here is your dashboard!");
+        chartCategory.setData(fillPieChart(loggedUser));
+        chartFood.setData(fillPieChart(loggedUser));
+        chartTransportation.setData(fillPieChart(loggedUser));
+        chartHousehold.setData(fillPieChart(loggedUser));
         try {
             NotificationPanelController.addNotificationPanel(headerPane, mainPane);
             NavPanelController.setup(drawer, menu);
@@ -40,15 +55,49 @@ public class HomepageController implements Initializable {
         }
     }
 
-    private static ObservableList<PieChart.Data> fillPieChart() {
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data("Food", 1),
-                        new PieChart.Data("Transportation", 2),
-                        new PieChart.Data("Household", 3)
-                );
+    private static ObservableList<PieChart.Data> fillPieChart(User user) {
+        ActivityQueries queries = new ActivityQueries(user.getActivities());
+        //        ArrayList<int[]> countCat = queries.getNrOfActivitiesByCat();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                                    new PieChart.Data("Food", 1),
+                                    new PieChart.Data("Transportation", 2),
+                                    new PieChart.Data("Household", 3)
+        );
 
-
+        //        if (type == 1) {
+        //            pieChartData = FXCollections.observableArrayList(
+        //                    new PieChart.Data("Food", countCat.get(0)[0]),
+        //                    new PieChart.Data("Transportation", countCat.get(0)[1]),
+        //                    new PieChart.Data("Household", countCat.get(0)[2])
+        //            );
+        //        } else {
+        //            pieChartData = FXCollections.observableArrayList(
+        //                    new PieChart.Data("Eat Vegetarian Meal", countCat.get(1)[0]),
+        //                    new PieChart.Data("Buy Organic Food", countCat.get(1)[0]),
+        //                    new PieChart.Data("Buy Locally Produced Food", countCat.get(1)[0]),
+        //                    new PieChart.Data("Buy Non-Processed Food", countCat.get(1)[0])
+        //            );
+        //        }
         return pieChartData;
+    }
+
+    /**
+     * .
+     * Sets the current logged in User to the one that was passed
+     *
+     * @param passedUser Logged in current user
+     */
+    public static void setUser(User passedUser) {
+        loggedUser = passedUser;
+    }
+
+    /**
+     * .
+     * Get the logged in User
+     *
+     * @return logged User
+     */
+    public static User getUser() {
+        return loggedUser;
     }
 }
