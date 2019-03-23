0908d3cb6c62db93f1f965d04e051a7f6b4684dc
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-03-23 19:29:20 +0100

    UseBikeInsteadOfCar functionality completed

--- src/main/java/data/UseBikeInsteadOfCar.java
+++ src/main/java/data/UseBikeInsteadOfCar.java
@@ -1,12 +1,14 @@
 package data;
 
+import tools.CarbonCalculator;
+
 import java.util.ArrayList;
 
 /**
  * Activity: Travel by bike instead of travelling by car.
  * @author Kostas Lyrakis
  */
-public class UseBikeInsteadOfCar extends TransportationActivity{
+public class UseBikeInsteadOfCar extends TransportationActivity {
     /**
      * Constructor.
      */
@@ -18,26 +20,35 @@ public class UseBikeInsteadOfCar extends TransportationActivity{
     @Override
     public double calculateCarbonSaved(User user) {
         // calculate total carbon saved today
-        ArrayList<Activity> activitiesPerformedOnTheSameDay = getActivitiesOfTheSameTypePerformedInTheSameDay(user);
+        ArrayList<Activity> activitiesPerformedOnTheSameDay =
+                getActivitiesOfTheSameTypePerformedInTheSameDay(user);
         double totalCarbonSavedToday = 0;
-        for(Activity activity: activitiesPerformedOnTheSameDay) {
+        for (Activity activity : activitiesPerformedOnTheSameDay) {
             totalCarbonSavedToday += activity.getCarbonSaved();
         }
 
         // calculate daily carbon emissions
         double dailyCarbonEmissions = calculateDailyCarbonEmissions(user);
-
+        double maxC02SavedByThisActivity;
         if (user.getCarType().equals("small")) {
-            // TODO
-            return 0;
+            //
+            maxC02SavedByThisActivity = CarbonCalculator.smallCarEmissions(this.getKilometres());
         } else if (user.getCarType().equals("medium")) {
             // TODO
-            return 0;
+            maxC02SavedByThisActivity = CarbonCalculator.mediumCarEmissions(this.getKilometres());
         } else if (user.getCarType().equals("large")) {
             // TODO
-            return 0;
+            maxC02SavedByThisActivity = CarbonCalculator.largeCarEmissions(this.getKilometres());
         } else {
-            return 0;
+            maxC02SavedByThisActivity = 0;
         }
+
+        if (dailyCarbonEmissions > maxC02SavedByThisActivity + totalCarbonSavedToday) {
+            return  maxC02SavedByThisActivity;
+        } else {
+            return dailyCarbonEmissions - totalCarbonSavedToday;
+        }
+
     }
+
 }
