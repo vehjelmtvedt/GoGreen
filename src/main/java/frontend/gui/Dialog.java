7f3f859d3a0b0f6a4ae7742e8626c8e9a9c556b8
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-03-24 19:59:11 +0100

    solved checkystyle errors

--- src/main/java/frontend/gui/Dialog.java
+++ src/main/java/frontend/gui/Dialog.java
@@ -27,7 +27,7 @@ public class Dialog {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Dialog.class.getResource("/frontend/fxmlPages/Dialog.fxml"));
 
-        JFXDialog dialog = loader.load();
+        final JFXDialog dialog = loader.load();
 
         DialogController controller = loader.getController();
         controller.setHeading(headerText);
