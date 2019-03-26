02ee6f2c8479186f78bf51f55ffe4c0238d3e9be
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-26 21:25:05 +0100

    Reorder pom.xml, add profile to WebConfig

--- src/main/java/backend/WebConfig.java
+++ src/main/java/backend/WebConfig.java
@@ -1,10 +1,12 @@
 package backend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Profile("prod")
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
     @Override
