3fe2f13f616de5b21f0c127b8d00cf669fce10c3
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-29 22:06:51 +0100

    UserThread checkstyle fixes

--- src/main/java/tools/UserThread.java
+++ src/main/java/tools/UserThread.java
@@ -6,20 +6,24 @@ import data.UserPendingData;
 import tools.SyncUserTask;
 
 public class UserThread extends Thread {
-    public static void main(String[] args) {
-        LoginDetails loginDetails = new LoginDetails("test", "qwerty");
-        User user = Requests.instance.loginRequest(loginDetails);
-        SyncUserTask syncUserTask = new SyncUserTask(Requests.instance, loginDetails, user);
-        UserThread userThread = new UserThread(syncUserTask);
-        userThread.run();
-    }
-
     private static final int sleepTime = 5000;
 
     private SyncUserTask syncUserTask;
 
     public UserThread(SyncUserTask syncUserTask) {
         this.syncUserTask = syncUserTask;
+    }
+
+    /**.
+     * Test method (to be deleted)
+     * @param args - Command line arguments
+     */
+    public static void main(String[] args) {
+        LoginDetails loginDetails = new LoginDetails("test", "qwerty");
+        User user = Requests.instance.loginRequest(loginDetails);
+        SyncUserTask syncUserTask = new SyncUserTask(Requests.instance, loginDetails, user);
+        UserThread userThread = new UserThread(syncUserTask);
+        userThread.run();
     }
 
     @Override
