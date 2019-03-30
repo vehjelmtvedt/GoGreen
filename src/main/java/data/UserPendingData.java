cf4f9aa1c128b5f88f0e4f02e351d590cc56f204
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-30 14:36:09 +0100

    Add sync friends

--- src/main/java/data/UserPendingData.java
+++ src/main/java/data/UserPendingData.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 public class UserPendingData {
     private ArrayList<UserAchievement> achievements;
     private ArrayList<String> friendRequests;
+    private ArrayList<String> friends;
 
     public UserPendingData() {
         achievements = new ArrayList<>();
         friendRequests = new ArrayList<>();
+        friends = new ArrayList<>();
     }
 
     public ArrayList<UserAchievement> getAchievements() {
@@ -19,11 +21,19 @@ public class UserPendingData {
         return friendRequests;
     }
 
+    public ArrayList<String> getFriends() {
+        return friends;
+    }
+
     public void addNewAchievement(UserAchievement achievement) {
         achievements.add(achievement);
     }
 
     public void addNewRequest(String request) {
         friendRequests.add(request);
+    }
+
+    public void addNewFriend(String friend) {
+        friends.add(friend);
     }
 }
