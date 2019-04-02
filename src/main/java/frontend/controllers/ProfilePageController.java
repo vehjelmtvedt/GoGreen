daeaf13ac641174f53743a86570693095f7a46cc
Jean Louis Clemente <J.L.Celmente@student.tudelft.nl>
2019-04-02 02:35:41 +0200

    Added Acheivement Display

--- src/main/java/frontend/controllers/ProfilePageController.java
+++ src/main/java/frontend/controllers/ProfilePageController.java
@@ -4,7 +4,9 @@ import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXProgressBar;
 import com.jfoenix.controls.JFXSpinner;
+import data.Achievement;
 import data.User;
+import data.UserAchievement;
 import frontend.gui.ProfilePageLogic;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -14,11 +16,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -62,7 +70,7 @@ public class ProfilePageController implements Initializable {
     private Label score;
 
     @FXML
-    private Label   level;
+    private Label level;
 
     @FXML
     private JFXProgressBar levelProgress;
@@ -70,14 +78,14 @@ public class ProfilePageController implements Initializable {
     @FXML
     private JFXProgressBar carbonSavedProgress;
 
-//    @FXML
-//    private VBox com;
-//
-//    @FXML
-//    private VBox incom;
-//
-//    @FXML
-//    private ToolBar toolBar;
+    @FXML
+    private VBox com;
+
+    @FXML
+    private VBox incom;
+
+    @FXML
+    private ToolBar toolBar;
 
     public static void setUser(User user) {
         thisUser = user;
@@ -95,10 +103,8 @@ public class ProfilePageController implements Initializable {
         score.setText("Total\nCarbon\nSaved: " + thisUser.getTotalCarbonSaved());
         profilePicture.setImage(new Image("frontend/Pics/user.png"));
 
-        ObservableValue<Number> level = ObservableValue<Number>(5);
-
-
-        levelProgress.progressProperty().bind(thisUser.getProgress().getLevel());
+//        ObservableValue<Number> level = ObservableValue < Number > (5);
+//        levelProgress.progressProperty().bind(thisUser.getProgress().getLevel());
 
         try {
             NotificationPanelController.addNotificationPanel(headerPane, mainPane);
@@ -107,127 +113,91 @@ public class ProfilePageController implements Initializable {
             e.printStackTrace();
         }
 
+        int count = 0;
+
+        // for every completed achievement module  is created
+        // and added to a VBox small pics might be added later
+        for (int i = 0; i < thisUser.getProgress().getAchievements().size(); i++) {
+
+            count++;
+
+            HBox hbox = new HBox();
+
+            hbox.setSpacing(10.0);
+
+            ImageView achievementimage = new ImageView();
+
+            Image path = new Image("achievementsimages/" + thisUser.getProgress()
+                    .getAchievements().get(i).getId() + ".png");
+
+            achievementimage.setFitHeight(32);
+
+            achievementimage.setFitWidth(32);
+
+            achievementimage.setImage(path);
+
+            Text name = new Text(i + 1 + ") " + ProfilePageLogic.getNameString(
+                    thisUser.getProgress().getAchievements().get(i)));
+            name.setFill(Color.GREEN);
+
+            Text bonus = new Text(",Got: " + ProfilePageLogic.getBonusString(
+                    thisUser.getProgress().getAchievements().get(i)) + " Points");
+            Text date = new Text(",Completed On: " + ProfilePageLogic.getDateString(
+                    thisUser.getProgress().getAchievements().get(i)) + ".");
+
+            hbox.getChildren().addAll(achievementimage, name, bonus, date);
+
+            com.getChildren().add(hbox);
+
+        }
+
+        if (count == 0) {
+
+            Label noachiements = new Label("No completed achievements yet");
+            com.getChildren().add(noachiements);
+        }
+
+        for (Achievement a : ProfilePageLogic.getList()) {
+
+            HBox hbox = new HBox();
+
+            hbox.setSpacing(10.0);
+
+            if (!isComplete(a)) {
+
+                ImageView achievementimage1 = new ImageView();
+
+                String image = "achievementsimages/" + a.getId() + ".png";
+
+
+                Image path1 = new Image("achievementsimages/8.png");
+
+                achievementimage1.setFitHeight(32);
+                achievementimage1.setFitWidth(32);
+                achievementimage1.setImage(path1);
+
+                Text name = new Text(a.getName());
+                Text points = new Text(",Complete to Get: " + a.getBonus() + " points.");
+
+                hbox.getChildren().addAll(achievementimage1, name, points);
+                incom.getChildren().add(hbox);
+            }
+
+        }
+
     }
+
+    public static boolean isComplete(Achievement achievement) {
+        for (UserAchievement userAchievement : thisUser.getProgress().getAchievements()) {
+            if (achievement.getId() == userAchievement.getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 
-//        int count = 0;
-//
-//        // for every completed achievement module  is created
-//        // and added to a VBox small pics might be added later
-//        for (int i = 0; i < thisUser.getProgress().getAchievements().size(); i++) {
-//
-//            count++;
-//
-//            HBox hbox = new HBox();
-//
-//            hbox.setSpacing(10.0);
-//
-//            ImageView achievementimage = new ImageView();
-//
-//            Image path = new Image("achievementsimages/" + thisUser.getProgress()
-//                    .getAchievements().get(i).getId() + ".png");
-//
-//            achievementimage.setFitHeight(32);
-//
-//            achievementimage.setFitWidth(32);
-//
-//            achievementimage.setImage(path);
-//
-//
-//            Text name = new Text(i + 1 + ") " + ProfilePageLogic.getNameString(
-//                    thisUser.getProgress().getAchievements().get(i)));
-//            name.setFill(Color.GREEN);
-//
-//            Text bonus = new Text(",Got: " + ProfilePageLogic.getBonusString(
-//                    thisUser.getProgress().getAchievements().get(i)) + " Points");
-//            Text date = new Text(",Completed On: " + ProfilePageLogic.getDateString(
-//                    thisUser.getProgress().getAchievements().get(i)) + ".");
-//
-//            hbox.getChildren().addAll(achievementimage, name, bonus, date);
-//
-//            com.getChildren().add(hbox);
-//
-//        }
-//
-//        if (count == 0) {
-//
-//            Label noachiements = new Label("No completed achievements yet");
-//            anchorPanecom.getChildren().add(noachiements);
-//            anchorPanecom.setMinWidth(300.0);
-//            //completedtab.disableProperty();
-//
-//        }
-//
-//        for (Achievement a : ProfilePageLogic.getList()) {
-//
-//            HBox hbox = new HBox();
-//
-//            hbox.setSpacing(10.0);
-//
-//            if (!isComplete(a)) {
-//
-//                ImageView achievementimage1 = new ImageView();
-//
-//                String image = "achievementsimages/" + a.getId() + ".png";
-//
-//
-//                Image path1 = new Image("achievementsimages/8.png");
-//
-//                achievementimage1.setFitHeight(32);
-//
-//                achievementimage1.setFitWidth(32);
-//
-//                achievementimage1.setImage(path1);
-//
-//
-//                Text name = new Text(a.getName());
-//                Text points = new Text(",Complete to Get: " + a.getBonus() + " points.");
-//
-//
-//                hbox.getChildren().addAll(achievementimage1, name, points);
-//
-//                incom.getChildren().add(hbox);
-//            }
-//
-//        }
-//
-//    }
-
-//    /**
-//     * checks if an achievement is already completed.
-//     *
-//     * @param achievement to check
-//     * @return boolean flag
-//     */
-//    public static boolean isComplete(Achievement achievement) {
-//        for (UserAchievement userAchievement : thisUser.getProgress().getAchievements()) {
-//            if (achievement.getId() == userAchievement.getId()) {
-//                return true;
-//            }
-//
-//        }
-//        return false;
-//    }
-//
-//    /**
-//     * Set user to loged in User.
-//     *
-//     */
-//    public static void setUser() {
-//        setUser();
-//    }
-//
-//    /**
-//     * Set user to loged in User.
-//     *
-//     * @param user loged in user
-//     */
-//    public static void setUser(User user) {
-//        thisUser = user;
-//        //for testing
-//
-//    }
-//
 
 
 
--- src/main/java/frontend/gui/ProfilePageLogic.java
+++ src/main/java/frontend/gui/ProfilePageLogic.java
@@ -42,4 +42,33 @@ public class ProfilePageLogic {
 
     }
 
+    public static String getNameString(UserAchievement userAchievement) {
+
+        return list.get(userAchievement.getId()).getName();
+
+    }
+
+    /**
+     * this returns a String with Achievement bonus.
+     *
+     * @return bonus
+     */
+    public static int getBonusString(UserAchievement userAchievement) {
+
+        return list.get(userAchievement.getId()).getBonus();
+    }
+
+    /**
+     * this returns a String with Achievement completion date.
+     *
+     * @return date
+     */
+    public static String getDateString(UserAchievement userAchievement) {
+
+
+        return userAchievement.getDate().toString();
+
+    }
+
+
 }
