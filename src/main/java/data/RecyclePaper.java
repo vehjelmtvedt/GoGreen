169dff10a69d5c1b4b028d51befc7d9a785ddfda
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-04-05 13:02:48 +0200

    Recycle classes functionality is completed.

--- src/main/java/data/RecyclePaper.java
+++ src/main/java/data/RecyclePaper.java
@@ -11,8 +11,17 @@ public class RecyclePaper extends Activity {
         this.setCategory("Household");
     }
 
+    /**
+     * According to carbonindependent.org every user that recycles paper can save on average 70kg
+     * of carbon per year. That does not seem accurate enough because it assumes that regardless
+     * of a user's spending habits, the amount of carbon saved will be the same. This method needs
+     * to be updated in the future. This function calculates daily CO2 saved, so its use should
+     * be limited to once per day.
+     * @param user currently logged in user
+     * @return CO2 saved
+     */
     @Override
     public double calculateCarbonSaved(User user) {
-        return 0;
+        return 70.0 / 365.0;
     }
 }
--- src/main/java/data/RecyclePlastic.java
+++ src/main/java/data/RecyclePlastic.java
@@ -11,8 +11,17 @@ public class RecyclePlastic extends Activity {
         this.setCategory("Household");
     }
 
+    /**
+     * According to carbonindependent.org every user that recycles plastic can save on average 140kg
+     * of carbon per year. That does not seem accurate enough because it assumes that regardless
+     * of a user's spending habits, the amount of carbon saved will be the same. This method needs
+     * to be updated in the future. This function calculates daily CO2 saved, so its use should
+     * be limited to once per day.
+     * @param user currently logged in user
+     * @return daily CO2 saved.
+     */
     @Override
     public double calculateCarbonSaved(User user) {
-        return 0;
+        return 140.0 / 365.0;
     }
 }
