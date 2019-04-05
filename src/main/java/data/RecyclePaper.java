c7765f46c43b17d7ddb275722a9536ef819a985f
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-04-05 12:16:45 +0200

    added constructors to Recycle classes, added new functionality to the CarbonCalculator class so that it can return the carbon dioxide produced by a user's miscellaneous spending

--- src/main/java/data/RecyclePaper.java
+++ src/main/java/data/RecyclePaper.java
@@ -2,6 +2,14 @@ package data;
 
 public class RecyclePaper extends Activity {
 
+    /**
+     * Constructor.
+     */
+    public RecyclePaper() {
+        super();
+        this.setName("Recycle Paper");
+        this.setCategory("Household");
+    }
 
     @Override
     public double calculateCarbonSaved(User user) {
--- src/main/java/data/RecyclePlastic.java
+++ src/main/java/data/RecyclePlastic.java
@@ -1,6 +1,16 @@
 package data;
 
 public class RecyclePlastic extends Activity {
+
+    /**
+     * Constructor.
+     */
+    public RecyclePlastic() {
+        super();
+        this.setName("Recycle Plastic");
+        this.setCategory("Household");
+    }
+
     @Override
     public double calculateCarbonSaved(User user) {
         return 0;
