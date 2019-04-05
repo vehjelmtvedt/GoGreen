32a3b51c7c725d5f176eac0827c14af22e4c800d
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-04-05 20:06:54 +0200

    Remove unused imports for WebConfigs

--- src/main/java/backend/InsecureWebConfig.java
+++ src/main/java/backend/InsecureWebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.RestTemplate;
 
