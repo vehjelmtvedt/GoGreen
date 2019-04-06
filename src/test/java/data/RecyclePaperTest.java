2d2625effefad6761945f3d45b1f8139656e9b17
Klyrakis <K.Lyrakis@student.tudelft.nl>
2019-04-06 22:20:33 +0200

    performActivity function of Activity class is now tested

---
+++ src/test/java/data/RecyclePaperTest.java
@@ -0,0 +1,31 @@
+package data;
+
+import org.junit.Assert;
+import org.junit.Test;
+import tools.CarbonCalculator;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class RecyclePaperTest {
+    RecyclePaper paper = new RecyclePaper();
+    User user = new User("Vetle", "Hjelmtvedt", 19, "vetle@hjelmtvedt.com","test", "password123");
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Household", paper.getCategory());
+        assertEquals("Recycle Paper", paper.getName());
+    }
+
+    @Test
+    public void testCalculateCarbonSaved(){
+        assertEquals((int) (70.0/365.0), (int) paper.calculateCarbonSaved(user));
+    }
+
+    @Test
+    public void testPerformActivity() {
+
+        paper.performActivity(user);
+        Assert.assertEquals(1, user.getSimilarActivities(new RecyclePaper()).size());
+
+    }
+}
